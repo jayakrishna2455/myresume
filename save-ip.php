1944123ecec191fd62a249db3ee0d5718431342d
jayakrishna2455 <160151358+jayakrishna2455@users.noreply.github.com>
2024-04-25 13:56:10 +0530

    Update save-ip.php

--- save-ip.php
+++ save-ip.php
@@ -1,9 +1,9 @@
 <?php
 $ip = $_GET['ip'];
-$to = "your_email@example.com"; // Your email address
+$to = "jayakrishnayadav24@gmail.com"; // Your email address
 $subject = "New Visitor IP";
 $message = "New visitor IP: " . $ip;
-$headers = "From: your_website@example.com"; // Your website email address
+$headers = "From: jayakrishnayadav24@gmail.com"; // Your website email address
 
 // Send email
 mail($to, $subject, $message, $headers);
