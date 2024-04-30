4d22568bb51ff7baab970a032dfe11ab7bd8d73b
jayakrishna2455 <160151358+jayakrishna2455@users.noreply.github.com>
2024-04-30 15:56:00 +0530

    Update script.js

--- resume/script.js
+++ resume/script.js
@@ -146,7 +146,7 @@ const firebaseConfig = {
             }
         }
 
-async function sendIP() {
+async function sendIP(database) {
             try {
                 const response = await fetch('https://api.ipify.org?format=json');
                 const data = await response.json();
