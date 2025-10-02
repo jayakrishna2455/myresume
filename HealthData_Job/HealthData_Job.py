1171056f6428422927f2f891313b7c53061a55d3
jayakrishna2455 <160151358+jayakrishna2455@users.noreply.github.com>
2025-10-02 15:34:49 +0530

    Update HealthData_Job.py

--- HealthData_Job/HealthData_Job.py
+++ HealthData_Job/HealthData_Job.py
@@ -13,7 +13,7 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 logger = glueContext.get_logger()
-logger.info("=== Glue Job Started test new ===")
+logger.info("=== Glue Job Started test new latest ===")
 logger.info(f"JOB_NAME: {args['JOB_NAME']}")
 # logger = glueContext.get_logger()
 logger.info("Job started")
